31db927ff10858b4e3c338a09fa6822636821da1
Daan Seijnhaeve <r0594395@ucll.be>
2020-07-28 16:43:12 +0200

    Added button functionality and backend for our frontend

--- ImageAndTextToDatabase/MainWindow.xaml.cs
+++ ImageAndTextToDatabase/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,54 @@ namespace ImageAndTextToDatabase
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        { 
+            if (DatabaseSelectorComboBox.SelectedIndex == 0)
+            {
+                //checks if our textboxes are configured correctly.
+                PathValidations.Validations(TextBox_DatabasePath.Text, TextBox_OutputPath.Text);
+                //Creates a text file called first.txt in our output directory.
+                string outputPath = TextBox_OutputPath.Text + "First.txt";
+                
+                string[] entries = Directory.GetFileSystemEntries(TextBox_DatabasePath.Text, "*", System.IO.SearchOption.AllDirectories);
+                File.WriteAllLines(outputPath, entries);
+            }
+            else if (DatabaseSelectorComboBox.SelectedIndex == 1)
+            {
+                
+            }
+        }
+
+        private void BrowseFolderDatabaseButton_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedFile = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == true)
+            {
+                selectedFile = System.IO.Path.GetDirectoryName(openFileDialog1.FileName);
+            }
+            string selectedDirectory = selectedFile.Substring(0, selectedFile.LastIndexOf("\\") + 1);
+            TextBox_DatabasePath.Text = selectedDirectory;
+        }
+
+        private void BrowseFolderOutputButton_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedFile = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == true)
+            {
+                selectedFile = System.IO.Path.GetDirectoryName(openFileDialog1.FileName);
+            }
+            string selectedDirectory = selectedFile.Substring(0, selectedFile.LastIndexOf("\\") + 1);
+            TextBox_OutputPath.Text = selectedDirectory;
+
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
         }
     }
 }
