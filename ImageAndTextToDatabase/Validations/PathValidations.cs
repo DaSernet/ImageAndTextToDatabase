632dd10b265af08ba1953b37be0148ee4fbb19a4
Daan Seijnhaeve <r0594395@ucll.be>
2020-07-28 16:46:18 +0200

    Added path validations

---
+++ ImageAndTextToDatabase/Validations/PathValidations.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace FileHierachy2database.Validations
+{
+    class PathValidations
+    {
+        public static void Validations(String DatabasePath, String OutputPath)
+        {
+            string message = "";
+            if (String.IsNullOrEmpty(DatabasePath))
+            {
+                message = "Database path empty";
+            }
+
+            if (String.IsNullOrEmpty(OutputPath))
+            {
+                message = "Output path empty";
+            }
+
+            if (DatabasePath == OutputPath)
+            {
+                message = "Paths matching";
+            }
+
+            if (!String.IsNullOrEmpty(message))
+            {
+                MessageBox.Show(message, "Error");
+            }
+        }
+    }
+}
