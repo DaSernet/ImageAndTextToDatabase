e12fd68261ffe566fff1ae44871a25d0d53d305e
DaSernet <Daanseijnhaeve@gmail.com>
2020-07-30 00:40:54 +0200

    Added attribute identifier

---
+++ ImageAndTextToDatabase/Filters/AttributeFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace ImageAndTextToDatabase.Filters
+{
+    class AttributeFilter
+    {
+        public static void MatchAttribute(String line)
+        {
+            string[] attributeOptions = { "Auctions", "CollectedBy", "CollectedWhen", "Collection", "Coordinates", "Country", "EthnicGroup", "Exhibition", "Height", "Id" };
+        }
+    }
+}
