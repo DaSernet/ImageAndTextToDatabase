1ac74f8004f09fdca8b76897e78ad482d68206a5
DaSernet <Daanseijnhaeve@gmail.com>
2020-07-30 00:58:08 +0200

    Added filtering and matching on attribute from a text file

--- ImageAndTextToDatabase/Filters/AttributeFilter.cs
+++ ImageAndTextToDatabase/Filters/AttributeFilter.cs
@@ -10,6 +10,23 @@ namespace ImageAndTextToDatabase.Filters
         public static void MatchAttribute(String line)
         {
             string[] attributeOptions = { "Auctions", "CollectedBy", "CollectedWhen", "Collection", "Coordinates", "Country", "EthnicGroup", "Exhibition", "Height", "Id" };
+            string[] values = line.Split('=');
+
+            int counter = 0;
+            int counterMatch;
+            string attributeName;
+            foreach (string attributeOption in attributeOptions)
+            {
+                if (values[0] == attributeOption)
+                {
+                    counterMatch = counter;
+                    attributeName = values[0];
+                    Console.WriteLine(counterMatch);
+                    Console.WriteLine(attributeName);
+                }
+                counter++;
+            }
         }
     }
 }
+
