f36dc82d3a1289bad17b30507040475c0bbd6d1b
DaSernet <Daanseijnhaeve@gmail.com>
2020-07-30 15:06:00 +0200

    Added some comments and added our actual attributes

--- ImageAndTextToDatabase/Filters/AttributeFilter.cs
+++ ImageAndTextToDatabase/Filters/AttributeFilter.cs
@@ -9,12 +9,15 @@ namespace ImageAndTextToDatabase.Filters
     {
         public static void MatchAttribute(String line)
         {
-            string[] attributeOptions = { "Auctions", "CollectedBy", "CollectedWhen", "Collection", "Coordinates", "Country", "EthnicGroup", "Exhibition", "Height", "Id" };
+            string[] attributeOptions = { "acquiredfrom","acquisitiondate","additionalfeatures","artist","artistgender","artistsg","associatefeatures","auctions","calabashinfo","certificate","chefferie","clan","collectedby","collectedwhen","collection","commanditaire","comments","commgender","commonfeatures","commsg","condition","confidential","country","createdate","createdatemax","createdatemin","creditline","depth","diameter","donationfrom","ethnicgroup","exhibition","features","groups","hairinfo","height","id","inventory","kingdom","langgroup","length","medbeinfo","medbkinfo","medboinfo","medceinfo","medclinfo","medfeinfo","medfiinfo","medglinfo","medhoinfo","medirinfo","medium","medivinfo","medmainfo","medotinfo","medrainfo","medreinfo","medseedpodsinfo","medshinfo","medskinfo","medstinfo","medwoinfo","needbetter","objectgender","objectname","objectnameex","objectnamegn","objectposture","photocopy","photographer","photoinvnr","photoprov","pigmentinfo","provenance","public","publication","raaiid","region","restoration","ritualassoc","sitearcheo","structuralfeatures","tms","usage","village","web","weight","width","workshop","workshoplist","yaleid" };
             string[] values = line.Split('=');
+            //Our attribute is in values[0]
+            //Our data is in values[1]
 
             int counter = 0;
             int counterMatch;
             string attributeName;
+            
             foreach (string attributeOption in attributeOptions)
             {
                 if (values[0] == attributeOption)
