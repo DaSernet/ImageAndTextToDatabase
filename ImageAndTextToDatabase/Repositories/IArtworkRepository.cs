c70cec49ce7c4c21aadaa99c678faa9b0f043b89
DaSernet <Daanseijnhaeve@gmail.com>
2020-07-30 16:51:49 +0200

    Artwork interface added

---
+++ ImageAndTextToDatabase/Repositories/IArtworkRepository.cs
@@ -0,0 +1,18 @@
+﻿using ImageAndTextToDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageAndTextToDatabase.Repositories
+{
+    public interface IArtworkRepository
+    {
+        Artwork GetOne(string name);
+        IList<Artwork> GetAll();
+        void Add(Artwork artwork);
+        void Update(Artwork artwork);
+        void Delete(Artwork artwork);
+    }
+}
