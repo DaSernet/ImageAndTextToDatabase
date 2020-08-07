9d0206049f9aa0dd3b605bc827fa35d6de3b52ec
DaSernet <Daanseijnhaeve@gmail.com>
2020-08-07 16:02:45 +0200

    Added our image to byte[] converter

---
+++ ImageAndTextToDatabase/Functions/ImageToByte.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageAndTextToDatabase.Functions
+{
+    internal class ImageToByte
+    {
+        //gets our photo string
+        private byte[] GetPhoto(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            byte[] data = new byte[fs.Length];
+
+            fs.Read(data, 0, (int)fs.Length);
+            fs.Close();
+
+            return data;
+        }
+
+        private string ByteArrayToString(string path)
+        {
+            byte[] bytes = GetPhoto(path);
+            string result = "0x";
+
+            result += BitConverter.ToString(bytes).Replace("-", String.Empty);
+
+            return result;
+        }
+    }
+}
